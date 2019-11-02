0991dac573a6dd4fdfd123d92850e376c6dad088
robarmstrong96 <kylearmstrong96@outlook.com>
2019-11-01 21:17:23 -0400

    mostly done

--- FileIO.cpp
+++ FileIO.cpp
@@ -12,11 +12,27 @@
 using namespace std;
 
 int KP_FileIO::getFileContents(const std::string &filename, std::string &contents) {
-	//TODO fill in
+	std::ifstream inputFile; // Create input stream, open requested file
+	inputFile.open(filename);
+	std::string line;
+	if(inputFile.is_open() || inputFile.good()) { // Check if file is open
+		while (getline(inputFile, line, '\n')) { // Read per line, newline used as delimiter.
+			contents += line;
+		}
+		return SUCCESS;
+	}
+	contents.clear(); // Unable to open file, clear string contents.
+	return COULD_NOT_OPEN_FILE_TO_READ;
 }
 
 int KP_FileIO::writeVectortoFile(const std::string filename,std::vector<std::string> &myEntryVector) {
-	//TODO fill in
+	std::ofstream outputFile; // Create input stream, open requested file
+	outputFile.open(filename);
+	if (outputFile.is_open() || outputFile.good()) {
+		for (int i = 0; i < (int)myEntryVector.size(); i++) {
+			outputFile << myEntryVector[i] << "\n";
+		}
+		return SUCCESS;
+	}
+	return COULD_NOT_OPEN_FILE_TO_WRITE;
 }
-
-
